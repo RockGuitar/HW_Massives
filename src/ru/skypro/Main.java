1941c02109f6c2d4230cd3a207b0c613b134de6a
Никита <oleinik.nikita96@gmail.com>
2022-01-14 22:13:38 +0300

    Переделал задания 2 и 3. Кстати заметил, что в предлагаемом решении в скобочках цикла if сравнивается ЗНАЧЕНИЕ под индексом массива со значением длины массива, а должно быть что сравнивается индекс значения массива со значением длины массива.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -13,16 +13,52 @@ public class Main {
         long[] arrayThree = new long[]{249, 978655, 12315, 150, 1};
 
         //Задание 2
-        System.out.println(arrayOne[0] + "," + arrayOne[1] + "," + arrayOne[2]);
-        System.out.println(arrayTwo[0] + "," + arrayTwo[1] + "," + arrayTwo[2]);
-        System.out.println(arrayThree[0] + "," + arrayThree[1] + "," + arrayThree[2] + ","
-                + arrayThree[3] + "," + arrayThree[4] + "\n");
+        for (int x = 0; x < arrayOne.length; x++) {
+            if (x == arrayOne.length - 1) {
+                System.out.print(arrayOne[x] + "\n");
+                break;
+            }
+            System.out.print(arrayOne[x] + ", ");
+        }
+
+        for (int y = 0; y < arrayTwo.length; y++) {
+            if (y == arrayTwo.length - 1) {
+                System.out.print(arrayTwo[y] + "\n");
+                break;
+            }
+            System.out.print(arrayTwo[y] + ", ");
+        }
+        for (int z = 0; z < arrayThree.length; z++) {
+            if (z == arrayThree.length - 1) {
+                System.out.print(arrayThree[z] + "\n");
+                break;
+            }
+            System.out.print(arrayThree[z] + ", ");
+        }
+
 
         //Задание 3
-        System.out.println(arrayOne[2] + "," + arrayOne[1] + "," + arrayOne[0]);
-        System.out.println(arrayTwo[2] + "," + arrayTwo[1] + "," + arrayTwo[0]);
-        System.out.println(arrayThree[4] + "," + arrayThree[3] + "," + arrayThree[2] + ","
-                + arrayThree[1] + "," + arrayThree[0] + "\n");
+        for (int x = arrayOne.length - 1; x > -1; x--) {
+            if (x == 0) {
+                System.out.print(arrayOne[x] + "\n");
+                break;
+            }
+            System.out.print(arrayOne[x] + ", ");
+        }
+        for (int y = arrayTwo.length - 1; y > -1; y--) {
+            if (y == 0) {
+                System.out.print(arrayTwo[y] + "\n");
+                break;
+            }
+            System.out.print(arrayTwo[y] + ", ");
+        }
+        for (int z = arrayThree.length - 1; z > -1; z--) {
+            if (z == 0) {
+                System.out.print(arrayThree[z] + "\n");
+                break;
+            }
+            System.out.print(arrayThree[z] + ", ");
+        }
         //Задание 4
         for (int i = 0; i < arrayOne.length; i++) {
             if (arrayOne[i] % 2 != 0) {
